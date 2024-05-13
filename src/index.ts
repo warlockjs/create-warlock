a74982bc0cdefef882fc499ae2bc5140d60ed5b6
hassanzohdy <hassanzohdy@gmail.com>
2024-05-13 14:26:13 +0300

    "Initial Release ⚡️"

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
-import { getJsonFile } from "@mongez/fs";
-import { program } from "commander";
-import * as path from "path";
 import { fileURLToPath } from "url";
 import createNewApp from "./commands/create-new-app";
 
 const __filename = fileURLToPath(import.meta.url);
 
-const __dirname = path.dirname(__filename);
-
 export default function createApp() {
-  const packageJson: any = getJsonFile(
-    path.resolve(__dirname, "../", "package.json"),
-  );
-
-  program.version(packageJson["version"]);
-
   createNewApp();
 }
