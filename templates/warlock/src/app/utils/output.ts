405bdf9636e63c36cb0e266d4504007938ffcf1c
Hasan Zohdy <hassanzohdy@gmail.com>
2025-12-17 20:29:52 +0200

    "ُUsed esm version in bin executable instead of cjs version"

--- templates/warlock/src/app/utils/output.ts
+++ templates/warlock/src/app/utils/output.ts
@@ -1,5 +1,4 @@
 import type { FinalOutput } from "@warlock.js/core";
-import { UserOutput } from "app/users/output/user.output";
 
 /**
  * Merge the output with this function will return the base output details
@@ -11,8 +10,8 @@ export function withBaseOutputDetails(moreOptions: FinalOutput): FinalOutput {
     isActive: "boolean",
     createdAt: "date",
     updatedAt: "date",
-    createdBy: UserOutput,
-    updatedBy: UserOutput,
+    createdBy: "object",
+    updatedBy: "object",
     ...moreOptions,
   };
 }
