b1eda44d1396652f4497304a3dbe10d9483245d1
Hasan Zohdy <hassanzohdy@gmail.com>
2026-01-07 09:36:32 +0200

    Enhanced framework stability

--- src/helpers/paths.ts
+++ src/helpers/paths.ts
@@ -1,18 +1,17 @@
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
+const root = process.cwd();
 
-const __dirname = path.dirname(__filename);
+const packageRootPath = root + "/node_modules/create-warlock";
 
-console.log("Executing from", __dirname);
+console.log("Executing from", packageRootPath);
 
 export type Template = "warlock";
 
 export function template(templateName: Template): string {
-  return path.resolve(__dirname, "../", "../", "templates", templateName);
+  return path.resolve(packageRootPath, "templates", templateName);
 }
 
 export function packageRoot(...paths: string[]): string {
-  return path.resolve(__dirname, "../", "../", ...paths);
+  return path.resolve(packageRootPath, ...paths);
 }
