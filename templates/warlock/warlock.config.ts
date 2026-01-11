cc0cedaac11b3ef18315f6b780d3f5bbf7c83f3a
Hasan Zohdy <hassanzohdy@gmail.com>
2026-01-11 20:08:42 +0200

    "Ensured migration table exists before running migrations"

--- templates/warlock/warlock.config.ts
+++ templates/warlock/warlock.config.ts
@@ -1,8 +1,15 @@
-import { registerAuthCleanupCommand, registerJWTSecretGeneratorCommand } from "@warlock.js/auth";
+import {
+  authMigrations,
+  registerAuthCleanupCommand,
+  registerJWTSecretGeneratorCommand,
+} from "@warlock.js/auth";
 import { defineConfig } from "@warlock.js/core";
 
 export default defineConfig({
   cli: {
     commands: [registerJWTSecretGeneratorCommand(), registerAuthCleanupCommand()],
+  },
+  database: {
+    migrations: authMigrations,
   },
 });
